7f732b87b5d351a402d985888d6e8c03147782e0
nikolav <admin@nikolav.rs>
2023-07-01 06:40:11 +0100

    Sat Jul  1 07:40:11 CEST 2023

--- src/lib/utils/has.ts
+++ src/lib/utils/has.ts
@@ -1,3 +1,5 @@
+import { TPrimitive } from '../../types';
+
 const has_ = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default (node: any, key: string) => has_(Object(node), key);
+export default (node: any, key: TPrimitive) => has_(Object(node), key);

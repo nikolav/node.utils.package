64c77b561ca51ed6f32a738146e54bb87030ba4b
nikolav <admin@nikolav.rs>
2023-07-01 06:50:41 +0100

    v100

--- src/lib/utils/has.ts
+++ src/lib/utils/has.ts
@@ -2,4 +2,5 @@ import { TPrimitive } from '../../types';
 
 const has_ = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default (node: any, key: TPrimitive) => has_(Object(node), key);
+export default <TObject = any>(node: TObject, key: TPrimitive): boolean =>
+  has_(Object(node), key);

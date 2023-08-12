c24ed3a486dae6518f11ecf50e6d06a3ecb94bb9
nikolav <admin@nikolav.rs>
2023-08-12 09:10:43 +0100

    Sat Aug 12 10:10:43 CEST 2023

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as has } from './utils/has';
 export { default as forEach } from './utils/forEach';
 export { default as isNumeric } from './utils/is-numeric';
+export { default as idGen } from './utils/id-gen';
